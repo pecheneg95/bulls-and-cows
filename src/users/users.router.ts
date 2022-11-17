cf4774883b24a2e871b8d9652e46e44011091621
pecheneg95 <pechera20@gmail.com>
2022-11-17 18:48:14 +0300

    [Lecture 5] added basic users

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -1,7 +1,14 @@
 import * as express from 'express';
+import { Application } from 'express';
 
 import { UsersController } from './users.controller';
+import { UsersValidation } from './users.validation';
 
 const router = express.Router();
 
-router.get('/me', ...UsersController.getMe);
+router.get('/me', UsersController.meGet);
+router.get('/:userId/stats', ...UsersValidation.me, UsersController.statsGet);
+
+export function mountRouter(app: Application): void {
+  app.use('/users', router);
+}
