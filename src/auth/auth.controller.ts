4ed7c92256ae7aec1402bd77cae72f5505b94eb8
pecheneg95 <pechera20@gmail.com>
2022-11-17 18:18:41 +0300

    [Lecture 5] added basic auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
 export class AuthController {
-  static signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  static signUpPOST = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { email, password, username } = req.body;
 
-      console.log('Username: ', username);
-      console.log('Email: ', email);
-      console.log('Password: ', password);
+      console.log('Username: ', username)
+      console.log('Email: ', email)
+      console.log('Password: ', password)
 
       res.status(201).json({
         message: 'User has been created',
@@ -17,27 +17,25 @@ export class AuthController {
     }
   };
 
-  static login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  static loginPOST = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { email, password } = req.body;
 
-      console.log('Email: ', email);
-      console.log('Password: ', password);
+      console.log('Email: ', email)
+      console.log('Password: ', password)
 
       res.status(200).json({
         message: 'Authentification succeeded.',
-        token: 'token',
       });
     } catch (error) {
       next(error);
     }
   };
 
-  static logout = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  
+  static logoutPOST = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const userId = Number(req.userId);
-
-      console.log('userId: ', userId);
+      console.log('Logout')
 
       res.status(200).json({
         message: 'Logout succeeded.',
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,16 +1,15 @@
+
 import * as express from 'express';
 import { Application } from 'express';
-
-import { validatePayload } from '@middleware';
 
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
 
 const router = express.Router();
 
-router.post('/sign-up', ...AuthValidation.signup, validatePayload, AuthController.signUp);
-router.post('/login', ...AuthValidation.login, validatePayload, AuthController.login);
-router.post('/logout', AuthController.logout);
+router.post('/sign-up', ...AuthValidation.signup, AuthController.signUpPOST);
+router.post('/login', ...AuthValidation.login, AuthController.loginPOST);
+router.post('/logout', AuthController.logoutPOST);
 
 export function mountRouter(app: Application): void {
   app.use('/auth', router);
