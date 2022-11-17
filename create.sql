18aeee8e05af9a80a23f5052e27b1c084436f8bf
pecheneg95 <pechera20@gmail.com>
2022-11-17 12:53:35 +0300

    [Lesson 7] changed snake_case on camelCase codestyle, added FK for winnerId

--- create.sql
+++ create.sql
@@ -1,41 +1,42 @@
 CREATE TYPE USER_ROLE AS ENUM ('user', 'admin');
 
 CREATE TABLE users (
-id SERIAL NOT NULL PRIMARY KEY,
-username TEXT NOT NULL,
-password TEXT NOT NULL,
-email TEXT NOT NULL UNIQUE,
-role USER_ROLE NOT NULL,
-created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  id SERIAL NOT NULL PRIMARY KEY,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  role USER_ROLE NOT NULL,
+  createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TYPE GAME_STATUS AS ENUM ('created', 'playing', 'finished');
 
 CREATE TABLE games (
-id SERIAL NOT NULL PRIMARY KEY,
-creator_id int NOT NULL,
-opponent_id int NOT NULL,
-status GAME_STATUS NOT NULL,
-winnerId int,
-hidden_by_creator TEXT,
-hidden_by_opponent TEXT,
-hidden_length int NOT NULL,
-created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE,
-FOREIGN KEY (opponent_id) REFERENCES users(id) ON DELETE CASCADE
+  id SERIAL NOT NULL PRIMARY KEY,
+  creatorId int NOT NULL,
+  opponentId int NOT NULL,
+  status GAME_STATUS NOT NULL,
+  winnerId int,
+  hiddenByCreator TEXT,
+  hiddenByOpponent TEXT,
+  hiddenLength int NOT NULL,
+  createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (creatorId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (opponentId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (winnerId) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE steps (
-id SERIAL NOT NULL PRIMARY KEY,
-user_id int NOT NULL,
-game_id int NOT NULL,
-sequence int NOT NULL,
-value TEXT NOT NULL,
-bulls int NOT NULL,
-cows int NOT NULL,
-created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+  id SERIAL NOT NULL PRIMARY KEY,
+  userId int NOT NULL,
+  gameId int NOT NULL,
+  sequence int NOT NULL,
+  value TEXT NOT NULL,
+  bulls int NOT NULL,
+  cows int NOT NULL,
+  createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (gameId) REFERENCES games(id) ON DELETE CASCADE
 );
